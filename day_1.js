574c10dd47cb2a9c33536a7b712e2553529b8aad
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-02 00:03:33 -0500

    Move data to folder

--- day_1.js
+++ day_1.js
@@ -2,17 +2,11 @@
 const fs = require('fs');
 const logger = require('npmlog');
 logger.level = 'warn'
-const depths_string = fs.readFileSync('./day_1_input.txt', {encoding:'utf8', flag:'r'});
+const depths_string = fs.readFileSync('./data/day_1_input.txt', {encoding:'utf8', flag:'r'});
 const depth_list = depths_string.split('\r\n');
 
 // PART 1
 logger.warn(`RESULT1: ${find_num_times_increased(depth_list)}`);
-
-// answer was 1292
-// => i get 1291 for some reason
-// logger.info(arr)
-
-
 // PART 2
 logger.warn(`RESULT2: ${find_sliding_window_sum_increase(depth_list)}`);
 
@@ -23,10 +17,10 @@ function find_num_times_increased(arr) {
   // PART 1
   let result = 0
   for (let i = 0; i < number_arr.length - 1; i++) {
-    // logger.info(`COMPARED: ${arr[i]} ${arr[i + 1]}`)
+    logger.info(`COMPARED: ${arr[i]} ${arr[i + 1]}`)
     if (number_arr[i] < number_arr[i + 1]) {
       result += 1
-      // logger.info(`INCREMENTED TO ${result}`)
+      logger.info(`INCREMENTED TO ${result}`)
     }
   }
   return result
@@ -45,7 +39,6 @@ function find_sliding_window_sum_increase(arr) {
     const curr_subarr = number_arr.slice(i + 1, i + 1 + window_size)
     logger.info(`PREV ARR: ${prev_subarr}`)
     logger.info(`ARR: ${curr_subarr}`)
-    
     const prev_sum = prev_subarr.reduce((a, b) => a + b, 0)
     const curr_sum = curr_subarr.reduce((a, b) => a + b, 0)
     logger.info(`COMPARED: ${prev_sum} ${curr_sum}`)
