293977a6c6ce088407c4f4c860b9ff0e76426e1f
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-03 01:36:46 -0500

    Add part 2

--- day_2.js
+++ day_2.js
@@ -10,7 +10,7 @@ for (const line of arr_day_2_input) {
 // PART 1
 logger.warn(`RESULT1: ${find_horizontal_vertical_positions(split_arr_day_2_input)}`);
 // PART 2
-// logger.warn(`RESULT2: ${find_sliding_window_sum_increase(depth_list)}`);
+logger.warn(`RESULT2: ${find_depth_with_aim(split_arr_day_2_input)}`);
 
 
 /**
@@ -43,6 +43,35 @@ function find_horizontal_vertical_positions(array_of_arrays) {
   return result.horizontal * result.vertical
 }
 
+function find_depth_with_aim(array_of_arrays) {
+  // PART 2
+  const result = {
+    horizontal: 0,
+    depth: 0,
+    aim: 0
+  }
+  for (const blah of array_of_arrays) {
+    const direction = blah[0]
+    const value = parseInt(blah[1], 10)
+    logger.info(`AIM: ${result.aim} - ${direction} ${value}`)
+    switch (direction) {
+      case 'forward':
+        result.horizontal += value
+        result.depth += result.aim * value
+        break;
+      case 'up':
+        result.aim -= value
+        break;
+      case 'down':
+        result.aim += value
+        break;
+    }
+  }
+  logger.info(JSON.stringify(result))
+  return result.horizontal * result.depth
+}
+
 module.exports = {
-  find_horizontal_vertical_positions
+  find_horizontal_vertical_positions,
+  find_depth_with_aim
 }
--- day_2.test.js
+++ day_2.test.js
@@ -1,4 +1,4 @@
-const { find_horizontal_vertical_positions } = require("./day_2");
+const { find_horizontal_vertical_positions, find_depth_with_aim } = require("./day_2");
 test("Day 2 - Expected Data", () => {
   const data = [
     ["forward", 5],
@@ -9,4 +9,5 @@ test("Day 2 - Expected Data", () => {
     ["forward", 2],
   ]
   expect(find_horizontal_vertical_positions(data)).toBe(150);
+  expect(find_depth_with_aim(data)).toBe(900)
 });
