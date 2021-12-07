9deb8aa25ea8849b110892825a3331704262b6e3
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-12-06 20:42:56 -0500

    Part 2 & Fixed pass by reference from Part 1

--- day_6.py
+++ day_6.py
@@ -13,7 +13,7 @@ def forecast_population(num_days, initial_timer_list):
         if timer == 0:
             return reproduce_timer
         return timer - 1
-    timer_list = initial_timer_list
+    timer_list = initial_timer_list[:]
     for day in range(0, num_days):
         logger.info(f"Day {day} - {timer_list}")
         for i in range(0, len(timer_list)):
@@ -25,14 +25,28 @@ def forecast_population(num_days, initial_timer_list):
     return len(timer_list)
 
 def forecast_population2(num_days, initial_timer_list):
+    # TIMES OUT - memory overflow - not valid for higher numbers
+    # I have 16 GB of memory but it overflows still....implementation issue :(
     timer_list = np.array(initial_timer_list)
     for day in range(0, num_days):
         logger.info(f"Day {day} - {timer_list}")
         num_zeros = np.count_nonzero(timer_list == 0)
         timer_list -= 1
         timer_list[timer_list <= -1] = reproduce_timer
-        timer_list = np.append(timer_list, np.full((1, num_zeros), new_child_reproduce_timer))
+        new_child_list = np.full((1, num_zeros), new_child_reproduce_timer)
+        timer_list = np.concatenate((timer_list, new_child_list), axis=None)
     return len(timer_list)
+
+def forecast_population3(num_days, initial_timer_list):
+    possible_ages = np.zeros(new_child_reproduce_timer + 1, dtype=np.int64)
+    # insert values into bins array
+    for timer in initial_timer_list:
+        possible_ages[timer] += 1
+    for day in range(num_days):
+        temp = possible_ages[0] # save count of fish that will be bred
+        possible_ages = np.roll(possible_ages, -1) # age fish
+        possible_ages[6] += temp # put parent fish back in timer
+    return possible_ages.sum()
 
 class TestValues(unittest.TestCase):
     def test_forecast_population(self):
@@ -45,6 +59,12 @@ class TestValues(unittest.TestCase):
         initial_timer_list = [3,4,3,1,2]
         self.assertEqual(forecast_population2(18, initial_timer_list), 26)
         self.assertEqual(forecast_population2(80, initial_timer_list), 5934)
+    
+    def test_forecast_population3(self):
+        initial_timer_list = [3,4,3,1,2]
+        self.assertEqual(forecast_population3(18, initial_timer_list), 26)
+        self.assertEqual(forecast_population3(80, initial_timer_list), 5934)
+        self.assertEqual(forecast_population3(256, initial_timer_list), 26984457539)
 
 if __name__ == '__main__':
     initial_fish_list = []
@@ -53,7 +73,7 @@ if __name__ == '__main__':
         initial_fish_list = [int(i) for i in lines[0].split(",")]
     population = forecast_population(80, initial_fish_list)
     logger.critical(f"PART 1: {population}")
-    population2 = forecast_population2(256, initial_fish_list)
+    population2 = forecast_population3(256, initial_fish_list)
     logger.critical(f"PART 2: {population2}")
     
     # python -m unittest test_add_fish_to_aquarium.py
